f39342ca0a53ab36ace6ae4039de1489d153b00a
Saurav Anand <sauravanand1608@gmail.com>
2025-11-11 16:44:56 +0530

    Added User Verify and Logout Route

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -6,6 +6,8 @@ const authController = require("../controller/authController");
 //User Routes
 router.post("/user/register",authController.registerUser);
 router.post("/user/login",authController.loginUser);
+router.get("/user/logout",authController.logoutUser);
+router.post("/user/verify", authController.verifyUser);
 
 
 module.exports=router;
