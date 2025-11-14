3a1f2c27531af4dc22aa08a09b23f6d67412689d
Saurav Anand <sauravanand1608@gmail.com>
2025-11-14 10:13:40 +0530

    Modified resume routes

--- server/src/routes/resume.js
+++ server/src/routes/resume.js
@@ -5,8 +5,8 @@ const verifyToken = require("../middleware/authMiddleware");
 
 //Resume Routes
 router.post("/resume/create", verifyToken, resumeController.createResume);
-router.get("/resume/:userId", resumeController.getResumeByUser);
-router.get("/resume/my-resume", verifyToken, resumeController.getUserResumes);
+router.get("/resume/my_resume", verifyToken, resumeController.getUserResumes);
+router.get("/resume/:userId",verifyToken, resumeController.getResumeByUser);
 router.put("/resume/:id", verifyToken, resumeController.updateResume);
 router.delete("/resume/:id", verifyToken, resumeController.deleteResume);
 
