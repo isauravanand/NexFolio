9dd9787d08c9eeac87a48d964c209d209c366eb6
Saurav Anand <sauravanand1608@gmail.com>
2025-11-16 17:26:27 +0530

    Created Dashboard Page

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Background from '../components/Background'
+
+const Dashboard = () => {
+  return (
+    <>
+    <Background>
+    <Navbar/>    
+    </Background>
+    
+    </>
+  )
+}
+
+export default Dashboard
