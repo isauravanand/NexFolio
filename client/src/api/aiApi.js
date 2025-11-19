2668f5a78186f48455b38ec094bfbcc250143a57
Saurav Anand <sauravanand1608@gmail.com>
2025-11-19 22:30:07 +0530

    Modified Ai Api coming from Backend

--- client/src/api/aiApi.js
+++ client/src/api/aiApi.js
@@ -1,21 +1,9 @@
-// import axios from "axios";
+import axios from "./axios";
 
-// export const generateAiResume = async (template) => {
-//     return axios.post(
-//         "/ai/generate-resume",
-//         { template },
-//         {
-//             withCredentials: true,
-//             responseType: "blob",  // IMPORTANT
-//         }
-//     );
-// };
-
-import api from "./axios";
-
-export const generateAiResume = (template) =>
-    api.post(
-        "/ai/generate-resume",
-        { template },
-        { responseType: "arraybuffer" } // PDF binary response
+export const generateAiResume = (template) => {
+    return axios.post(
+        `/ai/generate-resume`,
+        { template },       // only send template
+        { responseType: "blob" } // <-- important for PDF
     );
+};
