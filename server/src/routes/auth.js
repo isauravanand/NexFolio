d12f88c226262bb770208590120577d956736674
Saurav Anand <sauravanand1608@gmail.com>
2025-11-26 23:33:17 +0530

    Added Get current user route

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController");
+const verifyToken = require("../middleware/authMiddleware");
 
 
 //User Routes
-router.post("/user/register",authController.registerUser);
-router.post("/user/login",authController.loginUser);
-router.get("/user/logout",authController.logoutUser);
+router.post("/user/register", authController.registerUser);
+router.post("/user/login", authController.loginUser);
+router.get("/user/logout", authController.logoutUser);
 router.post("/user/verify", authController.verifyUser);
+router.get("/user/me", verifyToken, authController.getCurrentUser);
 
 
-module.exports=router;
+module.exports = router;
